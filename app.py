b5c76ff32d368bcea7197e6eb3172d88e7661405
Communist217 <Needforspeed1900@gmail.com>
2021-06-04 22:57:40 +0700

    commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+from flask import (
+    request,
+    jsonify
+)
+
+CORS(app)
+
+
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    print(file)
+    # test_dataset = load_dataset("common_voice", "vi", split="test[:2%]")
+    # processor = Wav2Vec2Processor.from_pretrained("leduytan93/Fine-Tune-XLSR-Wav2Vec2-Speech2Text-Vietnamese")
+    # model = Wav2Vec2ForCTC.from_pretrained("leduytan93/Fine-Tune-XLSR-Wav2Vec2-Speech2Text-Vietnamese")
+    # resampler = torchaudio.transforms.Resample(48_000, 16_000)
+    # #
+    # # Preprocessing the datasets.
+    # # We need to read the audio files as arrays
+    # def speech_file_to_array_fn(batch):
+    #     speech_array, sampling_rate = torchaudio.load(batch["path"])
+    #     batch["speech"] = resampler(speech_array).squeeze().numpy()
+    #     return batch
+    #
+    # test_dataset = test_dataset.map(speech_file_to_array_fn)
+    # inputs = processor(test_dataset["speech"][:2], sampling_rate=16_000, return_tensors="pt", padding=True)
+    # with torch.no_grad():
+    #     logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
+    # predicted_ids = torch.argmax(logits, dim=-1)
+    # print("Prediction:", processor.batch_decode(predicted_ids))
+    # print("Reference:", test_dataset["sentence"][:2])
+    return jsonify({
+        'status': 'success',
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run()
